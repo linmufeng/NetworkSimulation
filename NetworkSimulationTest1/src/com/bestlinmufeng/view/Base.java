e496b6854d69161aa4c2bbf2609ffd6b1081d75e
林沐风 <linmufeng@yeah.net>
2016-11-04 02:58:22 +0800

    NetworkSimulation's UI
    Demo by linmufeng 
    Date: 2016/11/04

---
+++ NetworkSimulationTest1/src/com/bestlinmufeng/view/Base.java
@@ -0,0 +1,12 @@
+package com.bestlinmufeng.view;
+
+import javax.swing.JLabel;
+
+public class Base extends JLabel{
+    public static void main (String[] args){
+        Base base = new Base();
+    }
+    public Base(){
+        
+    }
+}
---
+++ NetworkSimulationTest1/src/com/bestlinmufeng/view/Vehicle.java
@@ -0,0 +1,13 @@
+package com.bestlinmufeng.view;
+
+import javax.swing.JLabel;
+
+public class Vehicle extends JLabel{
+    public static void main (String[] args){
+        Vehicle vehicle = new Vehicle();
+    }
+    
+    public Vehicle(){
+        
+    }
+}
